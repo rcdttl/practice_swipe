08d9d25e4d3ba71f550f45ddac7b9e359fcb264c
rcdttl <rcdttl@gamil.com>
2020-11-13 17:17:09 +0900

    upload first

---
+++ android/app/src/main/java/example/practiseindicator201113/MainActivity.java
@@ -0,0 +1,6 @@
+package example.practiseindicator201113;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body:  test(),
+    );
+  }
+
+  final List<Widget> _pages = <Widget>[
+    new FlutterLogo(textColor: Colors.blue),
+    new FlutterLogo(style: FlutterLogoStyle.stacked, textColor: Colors.red),
+    new FlutterLogo(style: FlutterLogoStyle.horizontal, textColor: Colors.green),
+  ];
+
+  Widget test(){
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: new SizedBox(
+        height: 300.0,
+        child: Swiper(
+            outer: true,
+            scale:0.9,
+            fade:0.8,
+            viewportFraction: 0.8,
+            itemBuilder: (c, i) {
+              return new Card(
+                color: Colors.grey,
+                child: Text("$i"),
+              );
+            },
+            pagination: new SwiperPagination(
+                margin: new EdgeInsets.all(0.0),
+                builder: new SwiperCustomPagination(builder:
+                    (BuildContext context, SwiperPluginConfig config) {
+                  return new Container(
+                    child: new Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        new Container(
+                          child: new Align(
+                            alignment: Alignment.center,
+                            child: new DotSwiperPaginationBuilder(
+                                color: Colors.black12,
+                                activeColor: Colors.black,
+                                size: 10.0,
+                                activeSize: 20.0)
+                                .build(context, config),
+                          ),
+                        ),
+                        new Text(
+                          " ${config.activeIndex + 1}/${config.itemCount}",
+                          style: TextStyle(fontSize: 15.0),
+                        ),
+                      ],
+                    ),
+                    constraints: new BoxConstraints.expand(height: 50.0),
+                  );
+                })),
+            itemCount: 10),
+      ),
+    );
+  }
+}
